8040090257421074625e19d76d45719ab7235b38
Tiago Rodrigues <tiagoasrodrigues@protonmail.com>
2023-03-02 14:40:08 +0000

    :tada: Initial project

---
+++ src/Home/index.js
@@ -0,0 +1,14 @@
+import './styles.css'
+
+export function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <p>
+          GitHub Codespaces <span className="heart">♥️</span> React
+        </p>
+      </header>
+    </div>
+  )
+};
